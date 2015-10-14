c8893428de6dcd29d1e130afedc98261f7c4f7bf
Cory Johnson <coryaj@byu.edu>
2015-10-14 08:37:57 -0600

    Add basic test

---
+++ test/all.js
@@ -0,0 +1,6 @@
+var assert = require('assert')
+
+exports['test that stops execution on first failure'] = function() {
+    //assert.equal(2 + 2, 5, 'assert fails and test execution stop here')
+    assert.equal(3 + 2, 5, 'will never pass this since test failed above')
+}();
