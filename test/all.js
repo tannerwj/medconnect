f3e0ea45a06a0f0273e115d1ddbb3746f4e24bdd
Tanner Johnson <tannerwj@gmail.com>
2016-03-23 21:20:33 -0600

    added error msg on login, removed back button on home pages

--- test/all.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert')
-
-exports['test that stops execution on first failure'] = function() {
-    //assert.equal(2 + 2, 5, 'assert fails and test execution stop here')
-    assert.equal(3 + 2, 5, 'will never pass this since test failed above')
-}();
